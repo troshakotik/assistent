9c25e48ca022619f613617b04d51a2689dfac7b5
trofim <troshakotik@gmail.com>
2024-06-20 14:57:44 +0300

    deleted:    Program-Script.ahk 	deleted:    commands2.py 	deleted:    temp_audio.mp3 	deleted:    tests/commands_test.py 	deleted:    voice_over.py 	deleted:    wake_word.py

--- Program-Script.ahk
+++
@@ -1,9 +0,0 @@
-﻿#z::Run "https://www.autohotkey.com"  ; Win+Z
-
-^!n::  ; Ctrl+Alt+N
-{
-    if WinExist("Untitled - Notepad")
-        WinActivate
-    else
-        Run "Notepad"
-}
--- tests/commands_test.py
+++
@@ -1,9 +0,0 @@
-COMMANDS_PATH = "../commands/jarvis/app/commands"
-SOUNDS_PATH = "./sounds"
-AUTOHOTKEY_PATH = r"C:\Program Files\AutoHotkey\v2\AutoHotkey.exe"
-
-
-import subprocess
-import os
-
-subprocess.run([AUTOHOTKEY_PATH,r"D:\python-projects\assistent\commands\jarvis\app\commands\browser\ahk\Run browser.ahk"])
